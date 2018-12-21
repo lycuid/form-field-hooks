0c2230303196507ea5f4a4402b0de45a964e70ab
Abhishek <abhishekk19@gmail.com>
2018-12-21 17:14:25 +0530

    work in progress.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const { resolve } = require('path');
 
 const fileBaseName = 'bundle';
 
 var exports = module.exports = { fileBaseName };
 
 exports.baseConfigs = {
-	entry: path.resolve('src', 'index.ts'),
+	entry: resolve('src', 'index.ts'),
 	output: {
 		libraryTarget: 'umd',
 		filename: `${fileBaseName}.js`
@@ -14,14 +14,14 @@ exports.baseConfigs = {
 		rules: [
 			{
 				test: /\.ts|tsx?$/,
-				include: path.resolve(__dirname, '..'),
+				include: resolve(__dirname, '..'),
 				loader: 'awesome-typescript-loader'
 			}
 		]
 	},
 
 	resolve: {
-		modules: [path.resolve('node_modules'), path.resolve('src')],
+		modules: [resolve('node_modules'), resolve('src')],
 		extensions: ['.js', '.jsx', '.ts', '.tsx']
 	}
 }
